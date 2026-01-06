5db67b33594e2403703c540a3de028b015392096
Mejbah Uddin <mejbahuddin2004@gmail.com>
2026-01-06 22:01:47 +0800

    Question

---
+++ 3rd Semester/Data Structure and Algorithms/Chapter 2 er exercise 1-5/Question_No_1.c
@@ -0,0 +1,23 @@
+// 1: Write a algorithm/function to initialize an array of size 10 with the first 10 positive integers.
+#include <stdio.h>
+
+void displaydata(int arr[], int size){
+
+	for (int i = 0; i<size; i++){
+    	printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+}
+
+int main(){
+
+    int arr[10];
+
+    for (int i = 0; i<10; i++){
+        arr[i] = i+1;
+    }
+
+    displaydata(arr, 10);
+    return 0;
+}
