cc0f31616121556b4d8f2793d2232661bb755a0a
Mejbah Uddin <mejbahuddin2004@gmail.com>
2026-01-06 21:34:06 +0800

    Question 2

--- 3rd Semester/Data Structure and Algorithms/C_programming.c
+++ 3rd Semester/Data Structure and Algorithms/C_programming.c
@@ -1,16 +1,19 @@
+// Chapter 2 er exercise 1-5
+// question 2
 #include <stdio.h>
 
-void displaydata(int* arr[], int size){
+void displaydata(int arr[], int size){
 
 	for (int i = 0; i<size; i++){
-    	return arr[i];
+    	printf("%d ", arr[i]);
     }
+    printf("\n");
 
 }
 
 int insertdata(int arr[], int size, int element, int index){
 	
-    for(int i = size-1; i >= index; i++){
+    for(int i = size-1; i >= index; i--){
     	arr[i+1] = arr[i];
     }
     arr[index]= element;
@@ -22,6 +25,8 @@ int main(){
     int size = 5, element = 99, index = 3;
     displaydata(arr, size);
     insertdata(arr, size, element, index);
+    size = size+1;
     displaydata(arr, size);
+
 
 }
