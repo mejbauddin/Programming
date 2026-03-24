6e62a84a07146c2ba9e08e0d74df25d1bd55970c
Mejbah Uddin <mejbahuddin2004@gmail.com>
2026-03-24 15:32:04 +0800

    Class 4

--- 4th_Semester/Python/Class4/Class4.py
+++ 4th_Semester/Python/Class4/Class4.py
@@ -13,3 +13,24 @@ p = Person()
 
 p.randomf()
 p.sayHello()
+
+x = 100
+
+if x > 20:
+    print("Above ten")
+    if x > 24:
+        print("24")
+
+    elif x < 77:
+        print("77")
+    else: 
+        print("!!!!!!!!!!")
+
+
+fruits = ["apple","banana","cherry"]
+
+for x in fruits:
+    print(x)
+
+for x in "fruits":
+    print(x)
