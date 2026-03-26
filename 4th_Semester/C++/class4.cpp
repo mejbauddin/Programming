5069191bd9c64c30f3a70490c68afc009967da86
Mejbah Uddin <mejbahuddin2004@gmail.com>
2026-03-26 15:29:24 +0800

    c++

--- 4th_Semester/C++/class4.cpp
+++ 4th_Semester/C++/class4.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
+// FUnction Diclaration
+int myfunction(int num1, int num2){
+    int sum = num1 + num2;
+    return sum;
+}
+
+
 int main(){
+int a =10;
+int b = 30;
+int p = myfunction(a,b);
+cout<<"The Sum is :"<< p<<endl;
  // Array 
     // one dimensional
 int arr1[5];
