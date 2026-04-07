f9467fb291b2ffeb268ceba5785e9b1970fc7e14
Mejbah Uddin <mejbahuddin2004@gmail.com>
2026-04-07 16:25:03 +0800

    Python Class 6

---
+++ 4th_Semester/Python/class6/Lab2.py
@@ -0,0 +1,28 @@
+# Task 1. Create one variable of each type and print its type.
+
+a = 456
+b = 10.10
+c = False
+d = 'Mejabh'
+e = b'A'
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+
+# Task 2. Use constructor functions to create the same types.
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+print(d, type(d))
+
+# Task 3. Use underscores in large integer literals and print the values.
+big = 1_000_000_000_000
+hex_num = 0x_AA_BB_CC_EE
+print(big)
+print(hex_num)
+
+
