715f33ce4a4b66f62dbff5065ad5889f481ec5d7
Mejbah Uddin <mejbahuddin2004@gmail.com>
2026-04-16 14:11:29 +0800

    TCP-UDP-Client And Web Server python code

---
+++ 4th_Semester/Computer Networking/TCP-UDP-ClientAnd-WebServer/TCP-WebServer.py
@@ -0,0 +1,35 @@
+from socket import *
+import datetime
+
+serverPort = 12000
+serverSocket = socket(AF_INET,SOCK_STREAM)
+serverSocket.bind(('',serverPort))
+serverSocket.listen(10)
+print("Server is listening...")
+conn, addr = serverSocket.accept()
+
+     
+while True:
+    msg = conn.recv(1024).decode()
+    if not msg:
+        break
+
+    print(f"[{datetime.datetime.now()}] Client {addr} sent: {msg}")
+    if msg == "!time":
+        conn.send(str(datetime.datetime.now()).encode())
+    elif msg.startswith("!echo "):
+        serverSocket.send(msg[6:].encode())
+    elif msg == "!quit":
+        conn.send("Goodbye!".encode())
+        break
+    else:
+        response = f"Sending Time : {str(datetime.datetime.now())} \n Hello BOSS, You are asking me : {msg}"
+
+    conn.send(response.encode())
+
+conn.close()
+
+ 
+
+
+  
---
+++ 4th_Semester/Computer Networking/TCP-UDP-ClientAnd-WebServer/UDP-ClientServer.py
@@ -0,0 +1,13 @@
+from socket import *
+serverName = "localhost"
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+clientSocket.connect((serverName,serverPort))
+while True:
+    msg = input("Enter message: ")
+    clientSocket.send(msg.encode())
+    if msg == "!quit":
+        break
+    print("Server says:", clientSocket.recv(1024).decode())
+
+clientSocket.close()
